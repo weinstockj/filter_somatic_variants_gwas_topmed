2170f335b40cf955f55fd228de822096ea377f6d
Josh Weinstock <josh82093@gmail.com>
2021-06-21 17:33:59 -0400

    replace go run command with path to binary

--- filter_gwas_statistics.sh
+++ filter_gwas_statistics.sh
@@ -8,9 +8,11 @@ ac_threshold=$4 # recommend 150
 out=$5
 
 echo "filtering now"
-go run filter_gwas_results.go $1 $2 $3 | 
-    cut -f1,2,4,5,6,7,10,11,13 | 
-    awk -v threshold=$ac_threshold '{ if ($5 >= threshold) {print} }' | 
+# go run filter_gwas_results.go $1 $2 $3 | 
+binary=/net/topmed2/working/jweinstk/count_singletons/new_drivers/filter_somatic_variants_from_gwas/filter_gwas_results
+$binary $1 $2 $3 | 
+    cut -f1,2,4,5,6,7,9,10,11,13 | 
+    awk -v threshold=$ac_threshold 'NR==1;{ if ($5 >= threshold && ($7-$5) >= threshold) {print} }' | 
     bgzip -c > $5
 
 echo "tabixing now"
